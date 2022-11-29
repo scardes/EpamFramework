0e306b585458459a3895f1e23daa8ed90e53718a
scardes93 <scardes93@gmail.com>
2022-11-29 14:08:15 +0600

    Add new test and empty .runsettings

---
+++ EpamFramework/Tests/MailRu_AutorizationAndSendMail.cs
@@ -0,0 +1,103 @@
+﻿using EpamFramework.BusinessObjects;
+using EpamWebDriver;
+using EpamWebDriver.PageObjects;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+
+/// <summary>
+/// Selenium test project with two emails 
+/// first email on https://account.mail.ru/
+/// </summary>
+namespace ExampleService.Tests
+{
+    [TestFixture]
+    public class MailRu_AutorizationAndSendMail
+    {
+        public class NunitSetup
+        {
+            IWebDriver driver;
+
+            User testUserFail = new User("123", "123");
+            User testUserSuccess = new User("epamtestmail93@mail.ru", "EpamTest185");
+
+            [OneTimeSetUp]
+            public void Setup()
+            {
+                //Below code is to get the drivers folder path dynamically.
+                string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+                //Creates the ChomeDriver object, Executes tests on Google Chrome
+                driver = new ChromeDriver(path + @"\drivers\");
+            }
+
+            [Test, Order(1)]
+            public void LoadMailInBrowser_ClickSignUp_LoadSuccess()
+            {
+                // Open mail.ru page
+                var autorizationMailru = new MailRuAutorizationPageObjects(driver);
+                autorizationMailru.AutorizationInMailRU();
+
+                //Check window is loaded
+                Assert.AreEqual("Account Mail.Ru", driver.Title);
+            }
+
+            [Test, Order(2)]
+            public void Authorization_WithEmptyEmailPassword_AuthorizationError()
+            {
+                // Open mail.ru page again with empty login data
+                var autorizationMailru = new MailRuAutorizationPageObjects(driver);
+                autorizationMailru.AutorizationInMailRU("");
+
+                //Take a error of empty username/email address
+                autorizationMailru.AssertPopUpEmptyError();
+            }
+
+            [Test, Order(3)]
+            public void Authorization_WithErrorEmailPassword_AuthorizationError()
+            {
+                //  Open mail.ru page again for incorrent data
+                var autorizationMailru = new MailRuAutorizationPageObjects(driver);
+                autorizationMailru.AutorizationInMailRU(testUserFail);
+
+                //And catch error of authorization
+                autorizationMailru.AssertPopUpInputError();
+            }
+
+            [Test, Order(4)]
+            public void Authorization_WithCorrectEmailPassword_AuthorizationSuccess()
+            {
+                // Make full autorization on mail.ru
+                var autorizationMailru = new MailRuAutorizationPageObjects(driver);
+                autorizationMailru.AutorizationInMailRU(testUserSuccess);
+
+                //And success of authorization
+                Assert.AreEqual("Авторизация", driver.Title);
+            }
+
+            [Test, Order(5)]
+            public void SendEmail_WithSomeInformation_SendSuccess()
+            {
+                // Make full autorization on mail.ru
+                var autorizationMailru = new MailRuAutorizationPageObjects(driver);
+                autorizationMailru.AutorizationInMailRU(testUserSuccess);
+                WebDriverExtensions.WaitSomeInterval(5);
+
+                //Fill SendEmailFromMailRU with 1.email receiver 2.Theme of letter 3.letter content and then send email
+                MailRuSendMailPageObjects SendMail = new MailRuSendMailPageObjects(driver);
+                SendMail.SendEmailFromMailRU("testepammail@yandex.ru", "Test Letter From mail.ru", "Content of test letter");
+            }
+
+            // runs once after all tests finished
+            [OneTimeTearDown]
+            public void Dispose()
+            {
+                // close down the browser
+                driver.Quit();
+                driver.Dispose();
+            }
+        }
+    }
+}
