30e446a8ec677bf846577a4c685536bdec17078d
scardes93 <scardes93@gmail.com>
2022-12-15 11:13:07 +0600

    Add Readme and update Xpath

--- EpamFramework/PageObjects/MailRuAutorizationPageObjects.cs
+++ EpamFramework/PageObjects/MailRuAutorizationPageObjects.cs
@@ -13,12 +13,12 @@ namespace EpamWebDriver.PageObjects
         string MailRuUrl = "https://account.mail.ru/";
 
         //Page objects For MailRUAutorization 
-        private readonly By NextButton = By.CssSelector("[data-test-id=next-button]");
-        private readonly By SubmitButton = By.CssSelector("[data-test-id=submit-button]");
-        private readonly By UsernameField = By.Name("username");
-        private readonly By PasswordField = By.Name("password");
-        private readonly By PopUpEmptyError = By.CssSelector("[data-test-id=required]");
-        private readonly By PopUpInputError = By.CssSelector("[data-test-id=password-input-error]");
+        private readonly By UsernameField = By.XPath("//input[@name = 'username']");
+        private readonly By PasswordField = By.XPath("//input[@name = 'password']");
+        private readonly By NextButton = By.XPath("//button[@data-test-id ='next-button']");
+        private readonly By SubmitButton = By.XPath("//button [@data-test-id='submit-button']");
+        private readonly By PopUpEmptyError = By.XPath("//small[text()='Поле «Имя аккаунта» должно быть заполнено']");
+        private readonly By PopUpInputError = By.XPath("//div[text()='Неверный пароль, попробуйте ещё раз']");
 
         public MailRuAutorizationPageObjects(IWebDriver driver)
         {
---
+++ README.md
@@ -0,0 +1,9 @@
+C# WebDriver test automation project for Epam course
+
+====
+
+Run from command line with chrome (change path of files to your own): 
+vstest.console.exe --settings:D:\Scardes\source\repos\EpamFramework\EpamFramework\devChrome.runsettings D:\Scardes\source\repos\EpamFramework\EpamFramework\bin\Debug\netcoreapp3.1\EpamFramework.dll
+
+Run from command line with firefox(change path of files to your own):
+vstest.console.exe --settings:D:\Scardes\source\repos\EpamFramework\EpamFramework\devFirefox.runsettings D:\Scardes\source\repos\EpamFramework\EpamFramework\bin\Debug\netcoreapp3.1\EpamFramework.dll
